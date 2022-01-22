d12268a3a6125fd1cd2f64468760706a3a12f788
Rob Watkin <rob@letsbuild.com>
2022-01-22 08:55:53 +0100

    Tidy up test groupings

--- test/index.test.ts
+++ test/index.test.ts
@@ -81,210 +81,208 @@ describe(`Japer`, () => {
         expect(200)
       })
 
-      describe('Japer', () => {
-        describe('/document', () => {
-          it('should throw an error if the document does not exist', async () => {
-            await request(app)
-              .delete(`${url}/some-doc/1/document`)
-              .expect(404)
+      describe('/document', () => {
+        it('should throw an error if the document does not exist', async () => {
+          await request(app)
+            .delete(`${url}/some-doc/1/document`)
+            .expect(404)
+        })
+
+        it('should create a new document', async () => {
+          const result = await request(app)
+            .post(`${url}/some-doc/document`)
+            .send({ a: 1, b: 2 })
+            .expect(200)
+
+          await request(app)
+            .get(`${url}/some-doc/${result.body.id}/document`)
+            .expect(200)
+        })
+
+        it('should not be able to create a document called "document', async () => {
+          const result = await request(app)
+            .post(`${url}/document/document`)
+            .send({ a: 1, b: 2 })
+            .expect(400)
+        })
+
+        it('should fail to create a new document version with id', async () => {
+          await request(app)
+            .post(`${url}/some-not-created-doc/1/document`)
+            .send({ a: 1, b: 2 })
+            .expect(403)
+        })
+
+        it('should get a document with version and id', async () => {
+          const doc = { document: { a: 1, b: 2 } }
+          const id = (await request(app)
+            .post(`${url}/some-doc/document`)
+            .send(doc)
+            .expect(200)).body.id
+          const result = await request(app)
+            .get(`${url}/some-doc/${id}/document`)
+            .expect(200)
+
+          expect(result.body).toEqual({
+            id: id,
+            version: 0,
+            document: { a: 1, b: 2 }
           })
+        })
 
-          it('should create a new document', async () => {
-            const result = await request(app)
-              .post(`${url}/some-doc/document`)
-              .send({ a: 1, b: 2 })
-              .expect(200)
+        it('should not expose the extra object', async () => {
+          const doc = { a: 1, b: 2 }
+          const id = (await request(app)
+            .post(`${url}/some-doc/document`)
+            .send(doc)
+            .expect(200)).body.id
+          const result = await request(app)
+            .get(`${url}/some-doc/${id}/document`)
+            .expect(200)
 
-            await request(app)
-              .get(`${url}/some-doc/${result.body.id}/document`)
-              .expect(200)
-          })
+          expect(result.body.extra).toBe(undefined)
+        })
 
-          it('should not be able to create a document called "document', async () => {
-            const result = await request(app)
-              .post(`${url}/document/document`)
-              .send({ a: 1, b: 2 })
-              .expect(400)
-          })
+        it('should get several document', async () => {
+          const doc1 = { a: 1, b: 2 }
+          const doc2 = { c: 1, d: 2 }
 
-          it('should fail to create a new document version with id', async () => {
-            await request(app)
-              .post(`${url}/some-not-created-doc/1/document`)
-              .send({ a: 1, b: 2 })
-              .expect(403)
-          })
+          const id1 = (await request(app)
+            .post(`${url}/xsome-doc/document`)
+            .send({ document: doc1 })
+            .expect(200)).body.id
+          const id2 = (await request(app)
+            .post(`${url}/xsome-doc/document`)
+            .send({ document: doc2 })
+            .expect(200)).body.id
 
-          it('should get a document with version and id', async () => {
-            const doc = { document: { a: 1, b: 2 } }
-            const id = (await request(app)
-              .post(`${url}/some-doc/document`)
-              .send(doc)
-              .expect(200)).body.id
-            const result = await request(app)
-              .get(`${url}/some-doc/${id}/document`)
-              .expect(200)
+          const result1 = await request(app)
+            .get(`${url}/xsome-doc/${id1}/document`)
+            .expect(200)
+          const result2 = await request(app)
+            .get(`${url}/xsome-doc/${id2}/document`)
+            .expect(200)
 
-            expect(result.body).toEqual({
-              id: id,
-              version: 0,
-              document: { a: 1, b: 2 }
-            })
-          })
+          expect(result1.body).toEqual({ id: id1, version: 0, document: doc1 })
+          expect(result2.body).toEqual({ id: id2, version: 0, document: doc2 })
+        })
 
-          it('should not expose the extra object', async () => {
-            const doc = { a: 1, b: 2 }
-            const id = (await request(app)
-              .post(`${url}/some-doc/document`)
-              .send(doc)
-              .expect(200)).body.id
-            const result = await request(app)
-              .get(`${url}/some-doc/${id}/document`)
-              .expect(200)
+        it('should update an existing document', async () => {
+          const docVersion0 = { a: 1, b: 2 }
+          const docVersion1 = { a: 1, c: 3 }
+          const { id, version } = (await request(app)
+            .post(`${url}/some-doc/document`)
+            .send({ document: docVersion0 })
+            .expect(200)).body
 
-            expect(result.body.extra).toBe(undefined)
-          })
+          expect(version).toBe(0)
 
-          it('should get several document', async () => {
-            const doc1 = { a: 1, b: 2 }
-            const doc2 = { c: 1, d: 2 }
+          const result1 = await request(app)
+            .get(`${url}/some-doc/${id}/document`)
+            .expect(200)
 
-            const id1 = (await request(app)
-              .post(`${url}/xsome-doc/document`)
-              .send({ document: doc1 })
-              .expect(200)).body.id
-            const id2 = (await request(app)
-              .post(`${url}/xsome-doc/document`)
-              .send({ document: doc2 })
-              .expect(200)).body.id
+          expect(result1.body.version).toEqual(0)
 
-            const result1 = await request(app)
-              .get(`${url}/xsome-doc/${id1}/document`)
-              .expect(200)
-            const result2 = await request(app)
-              .get(`${url}/xsome-doc/${id2}/document`)
-              .expect(200)
+          await request(app)
+            .post(`${url}/some-doc/${id}/document`)
+            .send({ document: docVersion1, version: version })
+            .expect(200)
 
-            expect(result1.body).toEqual({ id: id1, version: 0, document: doc1 })
-            expect(result2.body).toEqual({ id: id2, version: 0, document: doc2 })
-          })
+          const result2 = await request(app)
+            .get(`${url}/some-doc/${id}/document`)
+            .expect(200)
 
-          it('should update an existing document', async () => {
-            const docVersion0 = { a: 1, b: 2 }
-            const docVersion1 = { a: 1, c: 3 }
-            const { id, version } = (await request(app)
-              .post(`${url}/some-doc/document`)
-              .send({ document: docVersion0 })
-              .expect(200)).body
+          expect(result2.body).toEqual({ id: id, version: 1, document: docVersion1 })
+        })
 
-            expect(version).toBe(0)
+        it('should delete a document if it exists', async () => {
+          const result = await request(app)
+            .post(`${url}/some-doc/document`)
+            .send({ a: 1, b: 2 })
+            .expect(200)
 
-            const result1 = await request(app)
-              .get(`${url}/some-doc/${id}/document`)
-              .expect(200)
+          await request(app)
+            .delete(`${url}/some-doc/${result.body.id}/document`)
+            .expect(200)
+        })
 
-            expect(result1.body.version).toEqual(0)
+        it('should fail if deleting non existant document', async () => {
+          await request(app)
+            .delete(`${url}/some-doc/99/document`)
+            .expect(404)
+        })
 
-            await request(app)
-              .post(`${url}/some-doc/${id}/document`)
-              .send({document: docVersion1, version: version })
-              .expect(200)
+        it('should handle multiple documents', async () => {
+          const document1 = { a: 1, b: 2 }
+          const document2 = { a: 1, c: 3 }
+          const document3 = { a: 1, d: 4 }
+          const document4 = { a: 1, e: 5 }
 
-            const result2 = await request(app)
-              .get(`${url}/some-doc/${id}/document`)
-              .expect(200)
+          const postResult1 = await request(app)
+            .post(`${url}/doc1/document`)
+            .send({ document: document1 })
+            .expect(200)
+          const postResult2 = await request(app)
+            .post(`${url}/doc2/document`)
+            .send({ document: document2 })
+            .expect(200)
+          const postResult3 = await request(app)
+            .post(`${url}/doc3/document`)
+            .send({ document: document3 })
+            .expect(200)
+          const postResult4 = await request(app)
+            .post(`${url}/doc3/document`)
+            .send({ document: document4 })
+            .expect(200)
 
-            expect(result2.body).toEqual({ id: id, version: 1, document: docVersion1 })
-          })
+          const result1 = await request(app)
+            .get(`${url}/doc1/${postResult1.body.id}/document`)
+            .expect(200)
+          const result2 = await request(app)
+            .get(`${url}/doc2/${postResult2.body.id}/document`)
+            .expect(200)
+          const result3 = await request(app)
+            .get(`${url}/doc3/${postResult3.body.id}/document`)
+            .expect(200)
+          const result4 = await request(app)
+            .get(`${url}/doc3/${postResult4.body.id}/document`)
+            .expect(200)
 
-          it('should delete a document if it exists', async () => {
-            const result = await request(app)
-              .post(`${url}/some-doc/document`)
-              .send({ a: 1, b: 2 })
-              .expect(200)
+          expect(result1.body.document).toEqual(document1)
+          expect(result2.body.document).toEqual(document2)
+          expect(result3.body.document).toEqual(document3)
+          expect(result4.body.document).toEqual(document4)
+        })
 
-            await request(app)
-              .delete(`${url}/some-doc/${result.body.id}/document`)
-              .expect(200)
-          })
+        it('should accept a new version of a document', async () => {
+          const firstDoc = { a: 1, b: 2 }
+          const secondDoc = { a: 1, c: 'foo', d: { dd: 1, dc: 3 } }
 
-          it('should fail if deleting non existant document', async () => {
-            await request(app)
-              .delete(`${url}/some-doc/99/document`)
-              .expect(404)
-          })
+          const { id, version } = (await request(app)
+            .post(`${url}/some-doc/document`)
+            .send({ document: firstDoc })
+            .expect(200)).body
 
-          it('should handle multiple documents', async () => {
-            const document1 = { a: 1, b: 2 }
-            const document2 = { a: 1, c: 3 }
-            const document3 = { a: 1, d: 4 }
-            const document4 = { a: 1, e: 5 }
+          expect(version).toBe(0)
 
-            const postResult1 = await request(app)
-              .post(`${url}/doc1/document`)
-              .send({ document: document1 })
-              .expect(200)
-            const postResult2 = await request(app)
-              .post(`${url}/doc2/document`)
-              .send({ document: document2})
-              .expect(200)
-            const postResult3 = await request(app)
-              .post(`${url}/doc3/document`)
-              .send({ document: document3})
-              .expect(200)
-            const postResult4 = await request(app)
-              .post(`${url}/doc3/document`)
-              .send({ document: document4})
-              .expect(200)
+          const getFirstVersionResult = await request(app)
+            .get(`${url}/some-doc/${id}/document`)
+            .expect(200)
 
-            const result1 = await request(app)
-              .get(`${url}/doc1/${postResult1.body.id}/document`)
-              .expect(200)
-            const result2 = await request(app)
-              .get(`${url}/doc2/${postResult2.body.id}/document`)
-              .expect(200)
-            const result3 = await request(app)
-              .get(`${url}/doc3/${postResult3.body.id}/document`)
-              .expect(200)
-            const result4 = await request(app)
-              .get(`${url}/doc3/${postResult4.body.id}/document`)
-              .expect(200)
+          expect(getFirstVersionResult.body.version).toBe(version)
+          expect(getFirstVersionResult.body.document).toEqual(firstDoc)
 
-            expect(result1.body.document).toEqual(document1)
-            expect(result2.body.document).toEqual(document2)
-            expect(result3.body.document).toEqual(document3)
-            expect(result4.body.document).toEqual(document4)
-          })
+          await request(app)
+            .post(`${url}/some-doc/${id}/document`)
+            .send({ document: secondDoc, version: version })
+            .expect(200)
 
-          it('should accept a new version of a document', async () => {
-            const firstDoc = { a: 1, b: 2 }
-            const secondDoc = { a: 1, c: 'foo', d: { dd: 1, dc: 3 } }
+          const getSecondVersionResult = await request(app)
+            .get(`${url}/some-doc/${id}/document`)
+            .expect(200)
 
-            const { id, version } = (await request(app)
-              .post(`${url}/some-doc/document`)
-              .send({ document: firstDoc })
-              .expect(200)).body
-
-            expect(version).toBe(0)
-
-            const getFirstVersionResult = await request(app)
-              .get(`${url}/some-doc/${id}/document`)
-              .expect(200)
-
-            expect(getFirstVersionResult.body.version).toBe(version)
-            expect(getFirstVersionResult.body.document).toEqual(firstDoc)
-
-            await request(app)
-              .post(`${url}/some-doc/${id}/document`)
-              .send({ document: secondDoc, version: version })
-              .expect(200)
-
-            const getSecondVersionResult = await request(app)
-              .get(`${url}/some-doc/${id}/document`)
-              .expect(200)
-
-            expect(getSecondVersionResult.body.version).toBe(1)
-            expect(getSecondVersionResult.body.document).toEqual(secondDoc)
-          })
+          expect(getSecondVersionResult.body.version).toBe(1)
+          expect(getSecondVersionResult.body.document).toEqual(secondDoc)
         })
 
         it('should handle many documents on the same key', async () => {
@@ -318,52 +316,7 @@ describe(`Japer`, () => {
         })
       })
 
-      it('should filter the documents', async () => {
-        const loops = 3
-        const postResults = [], results = []
-
-        if (storeName === 'memory') {
-          store.setFilter((envelope: Envelope): boolean => {
-            return envelope.document.type == 'good'
-          })
-        } else if (storeName === 'mongoDb') {
-          store.setFilter(() => {
-            return { 'document.type': 'good' }
-          })
-        } else {
-          throw new Error('Define a custom store filter for this test')
-        }
-
-        for (let i = 0; i < loops; i++) {
-          const type = i % 3 ? 'bad' : 'good'
-          const result = await request(app)
-            .post(`${url}/doc/document`)
-            .send({ document:{ type: type, a: i, b: i + 1 }})
-            .expect(200)
-          postResults.push(result)
-        }
-
-        for (let i = 0; i < loops; i++) {
-          const result = await request(app)
-            .get(`${url}/doc/${postResults[i].body.id}/document`)
-          if (result.status === 200) {
-            results.push(result)
-          }
-        }
-
-        expect(results.length).toBe(Math.floor((loops - 1) / 3) + 1)
-
-        const allDocsResult = await request(app)
-          .get(`${url}/doc/document`)
-          .expect(200)
-
-        expect(allDocsResult.body.length).toBe(Math.floor((loops - 1) / 3) + 1)
-
-        allDocsResult.body.forEach((envelope: Envelope) => expect(envelope.document.type).toBe('good'))
-      })
-
-
-      describe('PATCH /patch', () => {
+      describe('/patch', () => {
         it('should fail if the document does not exist', async () => {
           const result = await request(app)
             .patch(`${url}/some-doc/99/patch/0`)
@@ -380,9 +333,7 @@ describe(`Japer`, () => {
 
         //   expect(result.body.version).toBe(1)
         // })
-      })
 
-      describe('/patch', () => {
         it('should throw an error if the document does not exist', async () => {
           await request(app)
             .get(`${url}/some-doc/1/patch/0`)
